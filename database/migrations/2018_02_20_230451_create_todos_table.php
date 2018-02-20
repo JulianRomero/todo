adbc9c6f8d1a49b1069c0ee755e70c17483871a5
JulianRomero <jarospirit@gmail.com>
2018-02-20 15:20:27 -0800

    Initial Schema

---
+++ database/migrations/2018_02_20_230451_create_todos_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTodosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('todos', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('list_id')->unsigned();
+            $table->string('title', 80);
+            $table->timestamps();
+            $table->foreign('list_id')->references('id')->on('lists')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('todos');
+    }
+}
